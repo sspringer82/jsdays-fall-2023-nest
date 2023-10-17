fae833a493e9049619759cd53a209396e7832737
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-10-17 15:11:23 +0200

    Express middleware - morgan

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import morgan from './morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan);
 
   app.useGlobalPipes(new ValidationPipe());
 
@@ -13,6 +16,7 @@ async function bootstrap() {
     .setDescription('The most awesome library API description')
     .setVersion('1.0')
     .addTag('books')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
