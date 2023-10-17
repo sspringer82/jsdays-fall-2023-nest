778edd30576d7cfff963a1b3def9fca8c4e0fd55
Sebastian Springer <88656+sspringer82@users.noreply.github.com>
2023-10-17 16:03:19 +0200

    Aufgabe 6 Lösung

--- Aufgaben/Aufgabe6.md
+++ Aufgaben/Aufgabe6.md
@@ -3,4 +3,4 @@
 1. Füge die Helmet Middleware hinzu https://github.com/helmetjs/helmet
 2. Füge die Compression Middleware hinzu https://expressjs.com/en/resources/middleware/compression.html
 
-Das ganze passiert in der `main.ts`
+Das Ganze passiert in der `main.ts`
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import morgan from './morgan';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(morgan);
+  app.use(helmet());
+  app.use(compression(5));
+  app.use((request, response, next) => {
+    console.log('My 🙈 middleware');
+    next();
+  });
 
   app.useGlobalPipes(new ValidationPipe());
 
